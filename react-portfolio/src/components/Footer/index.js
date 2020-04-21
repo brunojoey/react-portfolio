426a53ccba61714356095d1827253b37299afcb2
Joey Bruno <brunojoey12@gmail.com>
2020-04-20 20:24:22 -0500

    Created React App with components and a JSON object.

---
+++ react-portfolio/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <p>Made by Joey Bruno || @copy; 2020</p>
+        </footer>
+    );
+};
+
+export default Footer;
