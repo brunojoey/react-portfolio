90d269691889075c145e013c690fa539e22364a5
Joey Bruno <brunojoey12@gmail.com>
2020-04-27 16:19:11 -0500

    Redid the buildRoutes file.

--- src/utils/buildRoutes.js
+++ src/utils/buildRoutes.js
@@ -1,60 +1,28 @@
 import React from "react";
-import Image from "react-bootstrap";
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import Portfolio from "../pages/Portfolio";
 
 function buildRoutes() {
     const path = process.env.PUBLIC_URL;
 
     const routes = [
         {
-            title: "Note Taker",
-            image: "../src/images/note-taker-demo.gif",
-            alt: "Note Taker Demo Gif",
-            link: "https://afternoon-tor-09367.herokuapp.com/"
+            component: {Home},
+            link: [path + "/", "/Home"]
         },
         {
-            title: "Password Generator",
-            image: "../src/images/password-generator-screenshot.jpg",
-            alt: "Password Generator Image",
-            link: "https://brunojoey.github.io/password-generator/"
+            component: {Portfolio},
+            link: [path + "/Portfolio"]
         },
         {
-            title: "README Generator",
-            image: "../src/images/readme-generator-demo.gif",
-            alt: "README Generator Demo Gif",
-            link: "https://github.com/brunojoey/readme-generator"
+            component: {Contact},
+            link: [path + "/Contact"]
         },
-        {
-            title: "Budget Tracker",
-            image: "../src/images/budget-tracker-demo.gif",
-            alt: "Budget Tracker Demo Gif",
-            link: "https://boiling-lake-39406.herokuapp.com/"
-        },
-        {
-            title: "Employment Management System",
-            image: "../src/images/employee-system-demo.gif",
-            alt: "Employee System Demo Gif",
-            link: "https://github.com/brunojoey/employment-management-system"
-        },
-        {
-            title: "Employee Manager",
-            image: "../src/images/employee-summary-screenshot.png",
-            alt: "Employee Summary Image",
-            link: "https://github.com/brunojoey/employee-summary"
-        }
+            
     ];
     
-    return (
-        routes.map(route => (
-        <div className="card projectBody">
-            <Image src={path + route.image} alt={route.alt} fluid />
-            <div className="card-body">
-                <h3 className="card-title">{route.title}</h3>
-                <a href={route.link} className="btn btn-primary link">Check it Out</a>
-            </div>
-        </div>
-
-    ))
-    );
+    return routes;
 };
 
 export default buildRoutes;
