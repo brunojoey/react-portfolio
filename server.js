f75ecbd50d2cc100cb873550159cefacb39e7504
Joey Bruno <brunojoey12@gmail.com>
2020-04-26 23:07:23 -0500

    Added get

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('hello');
+});
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
