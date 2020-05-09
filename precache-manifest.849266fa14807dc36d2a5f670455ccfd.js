0a1a9e9e82ca3e12c690d493d5cfd40f873657d3
Joey Bruno <brunojoey12@gmail.com>
2020-05-09 16:28:03 -0500

    Updates

--- precache-manifest.849266fa14807dc36d2a5f670455ccfd.js
+++ precache-manifest.849266fa14807dc36d2a5f670455ccfd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0b029d052ab1de0893a6f5e5ebeb0261",
+    "revision": "0d32919fc0811fe2422323eee86270d7",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "90b80383395888382564",
+    "revision": "2230cd07095ec948a96b",
     "url": "/react-portfolio/static/css/main.ff97198d.chunk.css"
   },
   {
-    "revision": "1de4a3a5905cdcd8d80b",
-    "url": "/react-portfolio/static/js/2.f77ba3b5.chunk.js"
+    "revision": "49b268ca6db31de7d5b0",
+    "url": "/react-portfolio/static/js/2.289f2606.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/react-portfolio/static/js/2.f77ba3b5.chunk.js.LICENSE.txt"
+    "url": "/react-portfolio/static/js/2.289f2606.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90b80383395888382564",
-    "url": "/react-portfolio/static/js/main.2543a609.chunk.js"
+    "revision": "2230cd07095ec948a96b",
+    "url": "/react-portfolio/static/js/main.f694bfb6.chunk.js"
   },
   {
     "revision": "37db3f26b7c0ea36aed0",
