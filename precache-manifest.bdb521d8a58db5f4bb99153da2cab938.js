0619ea8984944e000547775e5a22404c20e0ecec
Joey Bruno <brunojoey12@gmail.com>
2020-07-15 09:47:04 -0500

    Updates

---
+++ precache-manifest.bdb521d8a58db5f4bb99153da2cab938.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2454832eed5773c56c0153ba99e0a494",
+    "url": "/react-portfolio/ae51d43d3902d392e325.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/react-portfolio/ae51d43d3902d392e325.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "1669acce50fb1b14252e66d6fa6ce2fb",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f14dabdf5c9b27c76275",
+    "url": "/react-portfolio/static/css/main.121de68a.chunk.css"
+  },
+  {
+    "revision": "d57decc579c027192300",
+    "url": "/react-portfolio/static/js/2.d5f3e7c8.chunk.js"
+  },
+  {
+    "revision": "701364163004c819c2f7b712050a307d",
+    "url": "/react-portfolio/static/js/2.d5f3e7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14dabdf5c9b27c76275",
+    "url": "/react-portfolio/static/js/main.de377e4d.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  },
+  {
+    "revision": "7805e76f6ab5e21bfb3e96f9820a0900",
+    "url": "/react-portfolio/static/media/2019Catalog_RUGS-Digital-Sample.7805e76f.pdf"
+  },
+  {
+    "revision": "f188afa173d492deebd60d575924d130",
+    "url": "/react-portfolio/static/media/2019Catalog_TEXTILE-Digital-Sample.f188afa1.pdf"
+  },
+  {
+    "revision": "47760ea6b63c17432843c1d762b2943d",
+    "url": "/react-portfolio/static/media/joseph_bruno_resume.47760ea6.pdf"
+  }
+]);
